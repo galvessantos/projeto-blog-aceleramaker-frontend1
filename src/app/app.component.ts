65c3325817b75ef526e37098c4aa8c8246614774
galvessantos <gabrielww1@hotmail.com>
2025-05-03 18:48:24 -0300

    chore(app): configure root application component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'blog-pessoal';
+  title = 'blog-pessoal-angular';
 }
